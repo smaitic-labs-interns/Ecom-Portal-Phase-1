f4bcd318f786c66f464d92754e9be9eb2db48cee
jenish-gainju <jenish.gainju@smaitic.com>
2022-08-31 17:32:26 +0545

    chnages in nabar

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,11 +1,11 @@
-import NavBar from "../components/NavBar";
+// import NavBar from "../components/NavBar";
 // import List from "../components/List";
 export default function HomePage() {
   return (
     <div>
-      <NavBar />
+      {/* <NavBar /> */}
       <div>
-      <h1> ecommerce portal</h1>
+      <h1>welcome to ecommerce portal</h1>
       </div>
     </div>
   );
